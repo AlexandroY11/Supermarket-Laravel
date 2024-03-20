baba95373faabe0fa57af2cd7a1f683df77a2e5d
Alexandro11 <alexandroyule@gmail.com>
2024-03-20 18:02:30 -0500

    Footer

---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,6 @@
+<footer class="footer bg-dark">
+    <div class="container">
+        <span class="text-white">© 2024 Heroes - Proyecto Angular</span>
+        <span class="text-white">Author: Alexandro Yule Valencia</span>
+    </div>
+</footer>
