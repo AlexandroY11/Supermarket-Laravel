86b4d94adb2005da6aedccd498b1f15da45499f5
Alexandro11 <alexandroyule@gmail.com>
2024-03-21 10:52:30 -0500

    Insertando Mapa en contactanos

--- resources/views/sobre_nosotros/contactanos.blade.php
+++ resources/views/sobre_nosotros/contactanos.blade.php
@@ -6,4 +6,6 @@
 
     <h1>Bienvenidos a Contáctanos</h1>
 
+    <iframe src="https://www.google.com/maps/embed?pb=!4v1711036233606!6m8!1m7!1smvb9Ysv51Rl1oDU0id6b4g!2m2!1d3.447408424983843!2d-76.48564077972293!3f173.21!4f-3.510000000000005!5f0.7820865974627469" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d15930.393606299467!2d-76.48564077972293!3d3.447408424983843!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8e30a7ad41c3a28d%3A0x491980fbd0db57b1!2sCra.%2012a%20%2372a50%2C%20Siete%20de%20Agosto%2C%20Cali%2C%20Valle%20del%20Cauca!5e0!3m2!1ses!2sco!4v1711036291937!5m2!1ses!2sco" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
 @endsection
